b1eebd4be1418427989fb5049bac634712bd943d
Someshmore123 <someshmore2995@gmail.com>
2025-07-29 10:07:40 +0530

    assignment on variable
    assignment on variable

---
+++ variable-assignment/01-variable-assignment.js
@@ -0,0 +1,14 @@
+var cityName ="Pune";
+console.log("My city name is:"+cityName);
+
+var bestFriend ="Alex";
+console.log("My bestfriend name is:"+bestFriend);
+
+var countryName ="India";
+console.log("My country name is:"+countryName);
+
+var schoolName ="New English school wanewadi";
+console.log("My school name is:"+schoolName);
+
+var collegeName ="SPCOET";
+console.log("My college name is:"+collegeName);
