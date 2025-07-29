1785127d8fe7f5a31e6ef063fdd65eea40f6abd4
Someshmore123 <someshmore2995@gmail.com>
2025-07-29 09:53:27 +0530

    assigment 2 on variable
    assigment 2 on variable

---
+++ 02-variable-assignment.js
@@ -0,0 +1,45 @@
+var myFullName;
+console.log("My full name is:"+myFullName);
+
+myFullName="Somesh More";
+console.log("My full name is:"+myFullName);
+
+
+myFullName="Somesh Dnyaneshwar More";
+console.log("My full name is:"+myFullName);
+
+console.log("---*----*----*----*-----*-----*-----*-----*-----*-----*-----*");
+
+var myLove="I love only JS";
+console.log("My love:"+myLove);
+
+console.log("---*----*----*----*-----*-----*-----*-----*-----*-----*-----*");
+
+var firstName="Somesh";
+console.log("First Name :"+firstName);
+
+var lastName="More";
+console.log("last Name :"+lastName);
+
+var age=22;
+console.log("Age:"+age);
+
+var Address="baramati,pune";
+console.log("Address :"+Address);
+
+var pinCode=412306;
+console.log("Pin Code :"+pinCode);
+
+var gender="male";
+console.log("Gender :"+gender);
+
+var isMarried="UnMarried";
+console.log("Is Married :"+isMarried);
+
+
+
+
+
+
+
+
